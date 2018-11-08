9871c8cdd9d2f3682b8a1a16992b249233b866a7
Richard Wilkes <wilkes@me.com>
2018-11-08 09:51:09 -0800

    Streamline

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -eo pipefail
-set -x
 
 CEF_VERSION=3.3538.1849.g458cc98
 
@@ -17,12 +16,7 @@ if [ -e cef/include/cef_version.h ]; then
 fi
 
 if [ $CEF_VERSION != "$EXISTING" ]; then
-    /bin/rm -rf cef
-    /bin/rm -f cef_binary_${CEF_VERSION}_${PLATFORM}_minimal.tar*
-    curl -LO http://opensource.spotify.com/cefbuilds/cef_binary_${CEF_VERSION}_${PLATFORM}_minimal.tar.bz2
-    # Have to break this into two steps because Windows fails to do them in one...
-    bunzip2 cef_binary_${CEF_VERSION}_${PLATFORM}_minimal.tar.bz2
-    tar xf cef_binary_${CEF_VERSION}_${PLATFORM}_minimal.tar
+    /bin/rm -rf cef cef_binary_${CEF_VERSION}_${PLATFORM}_minimal
+    curl -L http://opensource.spotify.com/cefbuilds/cef_binary_${CEF_VERSION}_${PLATFORM}_minimal.tar.bz2 | bunzip2 | tar xf -
     mv cef_binary_${CEF_VERSION}_${PLATFORM}_minimal cef
-    /bin/rm -f cef_binary_${CEF_VERSION}_${PLATFORM}_minimal.tar
 fi
