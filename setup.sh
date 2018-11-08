56241a9e7a8ceb89e99e2fe1357f61b628912ab6
Richard Wilkes <wilkes@me.com>
2018-11-07 17:15:10 -0800

    Fix inability of Windows to untar compressed files in one step

--- setup.sh
+++ setup.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -eo pipefail
+set -x
 
 CEF_VERSION=3.3538.1849.g458cc98
 
 # Setup PLATFORM
-case `uname -s` in
+case $(uname -s) in
     Darwin*)  PLATFORM=macosx64 ;;
     Linux*)   PLATFORM=linux64 ;;
-    Win*)     PLATFORM=windows64 ;;
-    *)        echo "Unsupported OS"; false ;;
+    MINGW64*) PLATFORM=windows64 ;;
+    *)        echo "Unsupported OS: $(uname -s)"; false ;;
 esac
 
 if [ -e cef/include/cef_version.h ]; then
@@ -17,8 +18,11 @@ fi
 
 if [ $CEF_VERSION != "$EXISTING" ]; then
     /bin/rm -rf cef
+    /bin/rm -f cef_binary_${CEF_VERSION}_${PLATFORM}_minimal.tar*
     curl -LO http://opensource.spotify.com/cefbuilds/cef_binary_${CEF_VERSION}_${PLATFORM}_minimal.tar.bz2
-    tar xzf cef_binary_${CEF_VERSION}_${PLATFORM}_minimal.tar.bz2
+    # Have to break this into two steps because Windows fails to do them in one...
+    bunzip2 cef_binary_${CEF_VERSION}_${PLATFORM}_minimal.tar.bz2
+    tar xf cef_binary_${CEF_VERSION}_${PLATFORM}_minimal.tar
     mv cef_binary_${CEF_VERSION}_${PLATFORM}_minimal cef
-    /bin/rm -f cef_binary_${CEF_VERSION}_${PLATFORM}_minimal.tar.bz2
+    /bin/rm -f cef_binary_${CEF_VERSION}_${PLATFORM}_minimal.tar
 fi
