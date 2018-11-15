a345e3d90c7445424e9be29c9bcd4fc9599056d3
Richard Wilkes <wilkes@me.com>
2018-11-15 09:49:53 -0800

    Add createMainArgs for non-Windows platforms

---
+++ internal/cef/cef_other.go
@@ -0,0 +1,25 @@
+// +build !windows
+
+package cef
+
+import (
+	// #cgo CFLAGS: -I ${SRCDIR}/../../cef
+	// #include "common.h"
+	"C"
+	"os"
+	"unsafe"
+)
+
+func createMainArgs() *C.cef_main_args_t {
+	args := (*C.cef_main_args_t)(C.calloc(1, C.sizeof_struct__cef_main_args_t))
+	if len(os.Args) > 0 {
+		cp := C.calloc(C.size_t(len(os.Args)), C.size_t(unsafe.Sizeof(uintptr(0))))
+		p := (*[1<<30 - 1]*C.char)(cp)
+		for i, arg := range os.Args {
+			p[i] = C.CString(arg)
+		}
+		args.argc = C.int(len(os.Args))
+		args.argv = (**C.char)(cp)
+	}
+	return args
+}
