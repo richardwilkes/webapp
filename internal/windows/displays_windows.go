62430f0e8b697297d40328207192cfb3b956d2b8
Joshua Achorn <wight@cock.li>
2019-01-18 11:22:13 -0500

    get DPI with physical monitor's device context

--- internal/windows/displays_windows.go
+++ internal/windows/displays_windows.go
@@ -19,19 +19,36 @@ func (d *driver) Displays() []*webapp.Display {
 			var dpiX, dpiY uint32
 			if err = GetDpiForMonitor(monitor, MDT_EFFECTIVE_DPI, &dpiX, &dpiY); err != nil {
 				jot.Error(err)
-			} else {
-				d.Bounds.X = float64(info.MonitorBounds.Left)
-				d.Bounds.Y = float64(info.MonitorBounds.Top)
-				d.Bounds.Width = float64(info.MonitorBounds.Right - info.MonitorBounds.Left)
-				d.Bounds.Height = float64(info.MonitorBounds.Bottom - info.MonitorBounds.Top)
-				d.UsableBounds.X = float64(info.WorkBounds.Left)
-				d.UsableBounds.Y = float64(info.WorkBounds.Top)
-				d.UsableBounds.Width = float64(info.WorkBounds.Right - info.WorkBounds.Left)
-				d.UsableBounds.Height = float64(info.WorkBounds.Bottom - info.WorkBounds.Top)
-				d.ScalingFactor = float64(dpiX) / 96
-				d.IsMain = info.Flags&MONITORINFOF_PRIMARY != 0
-				result = append(result, d)
+				// Windows 7 fallback
+				overallX := 0
+				overallY := 0
+
+				_hdc, _, _ := getDC.Call(uintptr(dc))
+
+				hdc := HDC(_hdc)
+
+				if hdc > 0 {
+					overallX = GetDeviceCaps(hdc, LOGPIXELSX)
+					overallY = GetDeviceCaps(hdc, LOGPIXELSY)
+					releaseDC.Call(uintptr(hdc))
+				}
+				if overallX > 0 && overallY > 0 {
+					dpiX = uint32(overallX)
+					dpiY = uint32(overallY)
+				}
 			}
+
+			d.Bounds.X = float64(info.MonitorBounds.Left)
+			d.Bounds.Y = float64(info.MonitorBounds.Top)
+			d.Bounds.Width = float64(info.MonitorBounds.Right - info.MonitorBounds.Left)
+			d.Bounds.Height = float64(info.MonitorBounds.Bottom - info.MonitorBounds.Top)
+			d.UsableBounds.X = float64(info.WorkBounds.Left)
+			d.UsableBounds.Y = float64(info.WorkBounds.Top)
+			d.UsableBounds.Width = float64(info.WorkBounds.Right - info.WorkBounds.Left)
+			d.UsableBounds.Height = float64(info.WorkBounds.Bottom - info.WorkBounds.Top)
+			d.ScalingFactor = float64(dpiX) / 96
+			d.IsMain = info.Flags&MONITORINFOF_PRIMARY != 0
+			result = append(result, d)
 		}
 		return 1
 	}, 0); err != nil {
--- internal/windows/shcoredll_windows.go
+++ internal/windows/shcoredll_windows.go
@@ -16,33 +16,12 @@ var (
 func GetDpiForMonitor(monitor HMONITOR, dpiType int32, dpiX, dpiY *uint32) error {
 	err := getDpiForMonitor.Find()
 	if err != nil {
-		return getDpiWindows7(monitor, dpiType, dpiX, dpiY)
+		return err
 	}
 
 	ret, _, err := getDpiForMonitor.Call(uintptr(monitor), uintptr(dpiType), uintptr(unsafe.Pointer(dpiX)), uintptr(unsafe.Pointer(dpiY)))
 	if ret != 0 {
 		return errs.NewWithCause(getDpiForMonitor.Name, err)
-	}
-	return nil
-}
-
-// Windows 7 compatible function to mimic GetDpiForMonitor
-func getDpiWindows7(monitor HMONITOR, dpiType int32, dpiX, dpiY *uint32) error {
-	overallX := 0
-	overallY := 0
-
-	_hdc, _, _ := getDC.Call(uintptr(0))
-
-	hdc := HDC(_hdc)
-
-	if hdc > 0 {
-		overallX = GetDeviceCaps(hdc, LOGPIXELSX)
-		overallY = GetDeviceCaps(hdc, LOGPIXELSY)
-		releaseDC.Call(uintptr(hdc))
-	}
-	if overallX > 0 && overallY > 0 {
-		*dpiX = uint32(overallX)
-		*dpiY = uint32(overallY)
 	}
 	return nil
 }
